040fd7776ca56ee96dfa2403cfd2a4cbf1b2666d
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 02:05:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(array)
     end
   i+=1 
   end
-  array.map {|ele| puts "#{ele.capitalize}!"}
+  array.map {|ele| puts ele.capitalize +"!"}
 end
 
 def long_planeteer_calls()
