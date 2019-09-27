8e9814b660ba4e50c5a8a61798de3e29f457cd42
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 02:10:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(name)
 end
 
 def summon_captain_planet(array)
-   planeteer_calls = []
+   calls = []
   array.collect do |calls|
     planeteer_calls << "#{calls.capitalize}!"
   end
