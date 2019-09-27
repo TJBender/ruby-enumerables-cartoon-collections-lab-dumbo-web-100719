18acdf1d429030cf5e5787b5fbe72e98bda7bcfb
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 02:15:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,6 @@ def long_planeteer_calls(array)
     return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
