ed19ff6b179eb87f714be239e676a43db565de09
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 02:21:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
     if cheeses.include?(cheese)
-      puts cheese
+      return cheese
     end
   end
   return nil
